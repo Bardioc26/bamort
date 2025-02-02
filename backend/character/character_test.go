8bf814d73ca56d9b91ac20c8a0fa9cfdc4811f36
Frank <frank@wuenscheonline.de>
2025-02-02 08:53:47 +0100

    solved circle imports

--- backend/character/character_test.go
+++ backend/character/character_test.go
@@ -3,7 +3,6 @@ package character
 import (
 	"bamort/database"
 	"bamort/equipment"
-	"bamort/importer"
 	"bamort/models"
 	"bamort/skills"
 	"encoding/base64"
@@ -784,43 +783,4 @@ func TestAddAusrüstung(t *testing.T) {
 		assert.NoError(t, err, "No error expected when adding Ausrüstung to char")
 	*/
 
-}
-
-func TestImportVTT2Char(t *testing.T) {
-	database.SetupTestDB()
-	fileName := fmt.Sprintf("../testdata/%s", "VTT_Import1.json")
-	char, err := importer.ImportVTTJSON(fileName)
-	assert.NoError(t, err, "expected no error when saving imported Char")
-	var chr2 Char
-	chr2.First(char.Name)
-	assert.Equal(t, char.ID, chr2.ID)
-	/*
-		// loading file to Modell
-		fileName := fmt.Sprintf("../testdata/%s", "VTT_Import1.json")
-		assert.Equal(t, "../testdata/VTT_Import1.json", fileName)
-		fileContent, err := os.ReadFile(fileName)
-		assert.NoError(t, err, "Expected no error when reading file "+fileName)
-		character := models.ImCharacterImport{}
-		err = json.Unmarshal(fileContent, &character)
-		assert.NoError(t, err, "Expected no error when Unmarshal filecontent")
-
-		assert.Equal(t, "Harsk Hammerhuter, Zen", character.Name)
-		assert.Equal(t, "Zwerg", character.Rasse)
-		assert.Equal(t, "Hören", character.Fertigkeiten[0].Name)
-		assert.Equal(t, 1, len(character.Zauber))
-		assert.Equal(t, 17, character.Lp.Value)
-		assert.Equal(t, 96, character.Eigenschaften.Gs)
-		assert.Equal(t, 74, character.Eigenschaften.Au)
-		assert.Equal(t, 1, len(character.Ausruestung))
-		assert.Equal(t, "Lederrüstung", character.Ausruestung[0].Name)
-		assert.Equal(t, "blau", character.Merkmale.Augenfarbe)
-		assert.Equal(t, "Lederrucksack", character.Behaeltnisse[0].Name)
-		assert.Equal(t, "Armbrust:schwer", character.Waffen[0].Name)
-		assert.Equal(t, 31, character.Ap.Value)
-		assert.Equal(t, "Armbrüste", character.Waffenfertigkeiten[0].Name)
-		assert.Equal(t, 3, len(character.Spezialisierung))
-		assert.Equal(t, "Kriegshammer", character.Spezialisierung[0])
-		assert.Equal(t, "Armbrust:schwer", character.Spezialisierung[1])
-		//fmt.Println(character)
-	*/
 }
---
+++ backend/importer/charimport_test.go
@@ -0,0 +1,49 @@
+package importer
+
+import (
+	"bamort/character"
+	"bamort/database"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestImportVTT2Char(t *testing.T) {
+	database.SetupTestDB()
+	fileName := fmt.Sprintf("../testdata/%s", "VTT_Import1.json")
+	char, err := ImportVTTJSON(fileName)
+	assert.NoError(t, err, "expected no error when saving imported Char")
+	var chr2 character.Char
+	chr2.First(char.Name)
+	assert.Equal(t, char.ID, chr2.ID)
+	/*
+		// loading file to Modell
+		fileName := fmt.Sprintf("../testdata/%s", "VTT_Import1.json")
+		assert.Equal(t, "../testdata/VTT_Import1.json", fileName)
+		fileContent, err := os.ReadFile(fileName)
+		assert.NoError(t, err, "Expected no error when reading file "+fileName)
+		character := models.ImCharacterImport{}
+		err = json.Unmarshal(fileContent, &character)
+		assert.NoError(t, err, "Expected no error when Unmarshal filecontent")
+
+		assert.Equal(t, "Harsk Hammerhuter, Zen", character.Name)
+		assert.Equal(t, "Zwerg", character.Rasse)
+		assert.Equal(t, "Hören", character.Fertigkeiten[0].Name)
+		assert.Equal(t, 1, len(character.Zauber))
+		assert.Equal(t, 17, character.Lp.Value)
+		assert.Equal(t, 96, character.Eigenschaften.Gs)
+		assert.Equal(t, 74, character.Eigenschaften.Au)
+		assert.Equal(t, 1, len(character.Ausruestung))
+		assert.Equal(t, "Lederrüstung", character.Ausruestung[0].Name)
+		assert.Equal(t, "blau", character.Merkmale.Augenfarbe)
+		assert.Equal(t, "Lederrucksack", character.Behaeltnisse[0].Name)
+		assert.Equal(t, "Armbrust:schwer", character.Waffen[0].Name)
+		assert.Equal(t, 31, character.Ap.Value)
+		assert.Equal(t, "Armbrüste", character.Waffenfertigkeiten[0].Name)
+		assert.Equal(t, 3, len(character.Spezialisierung))
+		assert.Equal(t, "Kriegshammer", character.Spezialisierung[0])
+		assert.Equal(t, "Armbrust:schwer", character.Spezialisierung[1])
+		//fmt.Println(character)
+	*/
+}

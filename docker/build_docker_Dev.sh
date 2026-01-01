79546d293200035b317c49beadf8b191fcf2ce9a
Frank <frank@wuenscheonline.de>
2026-01-01 12:36:45 +0100

    remove unneeded files

--- docker/build_docker_Dev.sh
+++
@@ -1,4 +0,0 @@
-# Build an image named vue_helper using the Setup.Dockerfile
-# The build args manage permissions when executing commands from inside the container
-#
-docker build -f ./dockerfiles/Dev.Dockerfile -t vue_app:dev vue_app

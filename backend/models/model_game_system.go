65d301542ec3e3f1259404a465f0477cf474acab
Frank <frank@wuenscheonline.de>
2026-01-25 22:41:20 +0100

    added structs functions

--- backend/models/model_game_system.go
+++ backend/models/model_game_system.go
@@ -1,5 +1,7 @@
 package models
 
+import "bamort/database"
+
 type GameSystem struct {
 	ID          uint   `gorm:"primaryKey"`
 	Code        string `gorm:"uniqueIndex;size:100;not null"`
@@ -13,4 +15,16 @@ type GameSystem struct {
 // TableName sets the table name for SchemaVersion
 func (GameSystem) TableName() string {
 	return "game_systems"
+}
+
+func (gs *GameSystem) FirstByCode(code string) error {
+	return database.DB.First(gs, "code = ?", code).Error
+}
+
+func (gs *GameSystem) GetDefault() error {
+	return database.DB.First(gs, "is_active = ?", true).Error
+}
+
+func (gs *GameSystem) FirstByName(name string) error {
+	return database.DB.First(gs, "name = ?", name).Error
 }
